524edfe9ab6bdfb3ff6c5fdd6cfb0ca7ec96c984
Bernard Murphy <bmurphy@gormanuniform.com>
2022-08-12 05:28:01 -0500

    first

---
+++ main.js
@@ -0,0 +1,18 @@
+const mysql = require('mysql');
+const mongo = require('mongodb').MongoClient;
+
+const sql = mysql.createConnection({
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DB
+}).connect();
+
+sql.query('SHOW TABLES', (err, results) => {
+    if (err) console.log('error', err);
+    else {
+        console.log(results);
+    }
+});
+
+sql.end();
---
+++ run.sh
@@ -0,0 +1,7 @@
+export MYSQL_PASSWORD='12Characters'
+export MYSQL_HOST='localhost'
+export MYSQL_USER='root'
+export MYSQL_DB='nanaimg_public'
+export MONGO_URL='mongodb://127.0.0.1:27017/nanaimg'
+export MONGO_DB='nanaimg'
+node main.js
