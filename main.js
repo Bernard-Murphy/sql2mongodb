558c3fdbb3d1451a6081fdbe9abde1068f2d888e
Bernard Murphy <bmurphy@gormanuniform.com>
2022-08-12 05:39:41 -0500

    table entries

--- main.js
+++ main.js
@@ -14,10 +14,22 @@ sql.query('SHOW TABLES', (err, results) => {
     if (err) console.log('error', err);
     else {
         const tables = [].slice.call(results);
-        for (let t = 0; t < tables.length; t++){
-            const table = tables[t][`Tables_in_${process.env.MYSQL_DB}`];
-            console.log(table);
-        }
+        mongo.connect(process.env.MONGO_URL, { useUnifiedTopology: true }, async (err, client) => {
+            if (err) console.log('MONGO CONNECT ERROR:\n', err);
+            else {
+                const db = client.db(process.env.MONGO_DB);
+                for (let t = 0; t < tables.length; t++){
+                    const table = tables[t][`Tables_in_${process.env.MYSQL_DB}`];
+                    console.log(`Dumping ${table}...`)
+                    sql.query(`SELECT * FROM ${table}`, (err, results) => {
+                        if (err) console.log(`An error occurred while connecting to ${table}`, err);
+                        else {
+                            console.log(results.length);
+                        }
+                    });
+                }
+            }
+        });
     }
 });
 
